b517e34514246d272c6d2612a30f2bafaad33fed
mdgacomunicacao <68704377+mdgacomunicacao@users.noreply.github.com>
2021-05-02 07:45:38 -0300

    Compartilhar em Redes Sociais Wordpress

---
+++ compartilhar-redes-sociais.php
@@ -0,0 +1,21 @@
+<div class="my-sm-share">
+  <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
+    <img src="/wp-content/uploads/2021/04/fb.svg">
+  </a>
+
+  <a target="_blank" href="https://twitter.com/share?text=<?php the_title(); ?>&amp;url=<?php the_permalink(); ?>">
+    <img src="/wp-content/uploads/2021/04/tt.svg">
+  </a>
+
+  <a target="_blank" href="https://api.whatsapp.com/send?text=<?php the_permalink(); ?>">
+    <img src="/wp-content/uploads/2021/04/wa.svg">
+  </a>
+
+  <a target="_blank" href="https://telegram.me/share/url?url=<?php the_permalink(); ?>">
+    <img src="/wp-content/uploads/2021/04/tg.svg">
+  </a>
+
+  <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php the_permalink(); ?>&amp;title=<?php the_title(); ?>">
+    <img src="/wp-content/uploads/2021/04/ld.svg">
+  </a>
+</div>
