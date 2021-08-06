841cf44687569677af4c18c748e9a1a188529eee
mdgacomunicacao <68704377+mdgacomunicacao@users.noreply.github.com>
2021-08-06 14:29:06 -0300

    Update option-page-acf.php

--- option-page-acf.php
+++ option-page-acf.php
@@ -17,11 +17,24 @@ if( function_exists('acf_add_options_page') ) {
 		'parent_slug'	=> 'footer',
 	));
 	
-	/*acf_add_options_sub_page(array(
+	/cf_add_options_sub_page(array(
 		'page_title' 	=> 'Header',
 		'menu_title'	=> 'Footer',
 		'parent_slug'	=> 'theme-general-settings',
-	));*/
+	));
 	
 }
+//COMO USAR OS CAMPOS DE OPTIONS PAGE
+
+/**
+Quando vai mostrar direto no codeblock a informação
+<?php the_field('nomedocampo',option) ?>
+
+Quando usa Dynamic Data no Oxygen
+1. Usar: PHP Funciont Return Value
+2. Em function name usar: get_field
+3. Em Function arguments usar: nome_do_campo, option
+
+**/
+
 ?>
