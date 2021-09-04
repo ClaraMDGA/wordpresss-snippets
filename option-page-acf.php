4f5d0db6e23ff4c1ec3111ad23350181a9b8e67b
mdgacomunicacao <68704377+mdgacomunicacao@users.noreply.github.com>
2021-09-04 11:15:43 -0300

    Update option-page-acf.php

--- option-page-acf.php
+++ option-page-acf.php
@@ -1,24 +1,23 @@
-<?php
-
 if( function_exists('acf_add_options_page') ) {
 	
+	
 	acf_add_options_page(array(
-		'page_title' 	=> 'Consultoria Mit', //change to the client name
-		'menu_title'	=> 'Consultoria Mit', //change to the client name
-		'menu_slug' 	=> 'consultoria-mit', //change to the client name without space and special chars
+		'page_title' 	=> 'Jumi', //change to the client name
+		'menu_title'	=> 'Jumi', //change to the client name
+		'menu_slug'	=> 'theme-general-settings', //change to the client name without space and special chars
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false,
 		'position' => 2,
 	));
 	
 	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Footer',
-		'menu_title'	=> 'Footer',
-		'parent_slug'	=> 'footer',
+		'page_title' 	=> 'Header',
+		'menu_title'	=> 'Header',
+		'parent_slug'	=> 'theme-general-settings',
 	));
 	
-	cf_add_options_sub_page(array(
-		'page_title' 	=> 'Header',
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Footer',
 		'menu_title'	=> 'Footer',
 		'parent_slug'	=> 'theme-general-settings',
 	));
@@ -29,12 +28,9 @@ if( function_exists('acf_add_options_page') ) {
 /**
 Quando vai mostrar direto no codeblock a informação
 <?php the_field('nomedocampo',option) ?>
-
 Quando usa Dynamic Data no Oxygen
 1. Usar: PHP Funciont Return Value
 2. Em function name usar: get_field
-3. Em Function arguments usar: nome_do_campo, option
-
+3. Em Function arguments usar: nome_do_campo,option
 **/
 
-?>
